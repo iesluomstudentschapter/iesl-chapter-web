00c5e5b98e4f5bc3f775e6b4cd44971bd78dc0ad
DARathnasiri <dilankar.17@cse.mrt.ac.lk>
2021-08-07 00:01:01 +0530

    registration-detail complete

--- iesl-chapter-web-frontend/src/app/pages/registration-details/registration-details.component.ts
+++ iesl-chapter-web-frontend/src/app/pages/registration-details/registration-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Registration } from 'src/app/models/registration';
+import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
   selector: 'app-registration-details',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationDetailsComponent implements OnInit {
 
-  constructor() { }
+  registrationId: any;
+
+  registrations: Registration[] = [];
+
+  registration: Registration = new Registration();
+
+  constructor(private route: ActivatedRoute, private _registrationService: RegistrationService) { }
 
   ngOnInit(): void {
+    this.registrationId = this.route.snapshot.paramMap.get('id');;
+
+    this._registrationService.getRegistrations()
+      .subscribe(
+        data => {
+          this.registration = data.filter(item => item.id == this.registrationId)[0];
+        }
+      );
   }
 
 }
